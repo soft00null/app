f189d14784f140f7fe3f2b76ef70f3bf10ae41fe
Glitch (glitch-hello-node) <none>
2022-08-09 09:49:20 +0000

    💓🌠 Checkpoint ./app.js:45676394/3155 ./test.js:45676394/1729

--- test.js
+++ test.js
@@ -8,23 +8,36 @@ var sheetdb = require('sheetdb-node');
                timeStyle: 'full'
            });
 
- let date = new Intl.DateTimeFormat('en-US', {
+ let date1 = new Intl.DateTimeFormat('en-US', {
                timeZone: "Asia/Kolkata",
                dateStyle: 'full' 
                //timeStyle: 'full'
            });
-let time = new Intl.DateTimeFormat('en-US', {
+let time1 = new Intl.DateTimeFormat('en-US', {
                timeZone: "Asia/Kolkata",
                //dateStyle: 'full' 
                timeStyle: 'medium'
            });
          
-    let indiaDate = date.format(datetime);
+    let indiaDate = date1.format(datetime);
     
-    let indiaTime = time.format(datetime);
+    let indiaTime = time1.format(datetime);
           
            console.log('India date: ' + indiaDate);
            console.log('India Time: ' + indiaTime);
+
+  // var date = datetime.toLocaleString('en-US', {
+  //   weekday: 'long',
+  //    year: 'numeric',
+ //     month: 'long',
+ //     day: 'numeric'
+ //   });
+
+ //   var time = datetime.toLocaleTimeString('en-US', {
+  //      hour:   '2-digit',
+  //      minute: '2-digit',
+  //      second: '2-digit',
+  //  });
 
 
 // create a config file
@@ -41,3 +54,11 @@ client.create({ Index: 1, Token: 1001, Name: "Mahesh", Phone: 9876543210, Date: 
 }, function(err){
   console.log(err);
 });
+
+
+// Adds single row to worksheet named "date"
+//client.create({ Index: 1, Token: 1001, Name: "Mahesh", Phone: 9876543210, Date: date , Time: time}, "Store B").then(function(data) {
+//  console.log(data);
+//}, function(err){
+//  console.log(err);
+//});
