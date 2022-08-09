45ca1fb6a4661d05731c2a9fbba44881d4990944
Glitch (glitch-hello-node) <none>
2022-08-09 10:24:24 +0000

    🛬🌌 Checkpoint ./app.js:45676394/3519

--- app.js
+++ app.js
@@ -53,7 +53,50 @@ app.post("/webhook", (req, res) => {
         headers: { "Content-Type": "application/json" },
         });
           // Add to google sheet code start
+          var sheetdb = require('sheetdb-node');
+
+ var datetime = new Date();
+
+   let intlDateObj = new Intl.DateTimeFormat('en-US', {
+               timeZone: "Asia/Kolkata",
+               dateStyle: 'full', 
+               timeStyle: 'full'
+           });
+
+ let date = new Intl.DateTimeFormat('en-US', {
+               timeZone: "Asia/Kolkata",
+               dateStyle: 'full' 
+               //timeStyle: 'full'
+           });
+let time = new Intl.DateTimeFormat('en-US', {
+               timeZone: "Asia/Kolkata",
+               //dateStyle: 'full' 
+               timeStyle: 'medium'
+           });
+         
+    let indiaDate = date.format(datetime);
+    
+    let indiaTime = time.format(datetime);
           
+           console.log('India date: ' + indiaDate);
+           console.log('India Time: ' + indiaTime);
+
+
+// create a config file
+var config = {
+  address: '0kzd0o37r10g4',
+};
+
+// Create new client
+var client = sheetdb(config);
+
+// Adds single row
+client.create({ Index: 1, Token: 1001, Name: from_name, Phone: from, Date: indiaDate , Time: indiaTime }, "Store A").then(function(data) {
+  console.log(data);
+}, function(err){
+  console.log(err);
+});
+
           // End of google sheet code
           
         }
