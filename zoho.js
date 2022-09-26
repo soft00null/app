0cfafbfddaf796ccc4eb68a28481deef855ba08d
Glitch (glitch-hello-node) <none>
2022-09-26 09:14:56 +0000

    🔦🏇 Checkpoint ./zoho.js:45676394/281

--- zoho.js
+++ zoho.js
@@ -1,11 +1,14 @@
 var axios = require('axios');
 var data = '';
 
+
+var centre = "Headquarter"; // Fetch centre from from MSG_BODY 
+
 var config = {
   method: 'get',
-  url: 'https://creator.zoho.in/api/v2/chatbot1234/test/report/All_Tasks',
+  url: 'https://creator.zoho.in/api/v2/chatbot1234/test/report/'+centre+'_Checkin',
   headers: { 
-    'Authorization': 'Zoho-oauthtoken 1000.aab0bc76289b0ceadde6b4a8961e7bd9.8d12627cb8fc6681a41a8b2758c5a856', 
+    'Authorization': 'Zoho-oauthtoken 1000.60bb258dc12e258e85773545948c1b0a.27afcb96e24b78256912620e2a478cc4', 
     'Cookie': 'ZCNEWLIVEUI=true; _zcsr_tmp=024af6fc-58a3-4e14-897b-ca50bf560c07; fa8dd4bb5a=3494010c8cb824229e44a51a4dce0868; zccpn=024af6fc-58a3-4e14-897b-ca50bf560c07'
   },
   data : data
