5f21a511ad4925196b44905647250c4159cd4273
Glitch (glitch-hello-node) <none>
2022-09-26 08:38:49 +0000

    🍤🐝 Checkpoint ./add.js:45676394/640

--- add.js
+++ add.js
@@ -6,10 +6,7 @@ var token = "1";
 var name = "Usha"
 var whatsapp = "+919873456798"
 
-var data = '{  "data": { "Date" : "Monday, 26 September 2022", "Time" : "1:48:03 PM", "Token" : "900001", "Name" : "Sneha", 
-      "WhatsApp" : "+919876543210"
-  }
-}';
+//var data = '{  "data": { "Date" : , "Time" : "1:48:03 PM", "Token" : "900001", "Name" : "Sneha", "WhatsApp" : "+919876543210" } }';
 
 var config = {
   method: 'post',
@@ -19,7 +16,16 @@ var config = {
     'Content-Type': 'text/plain', 
     'Cookie': 'ZCNEWLIVEUI=true; _zcsr_tmp=024af6fc-58a3-4e14-897b-ca50bf560c07; fa8dd4bb5a=3494010c8cb824229e44a51a4dce0868; zccpn=024af6fc-58a3-4e14-897b-ca50bf560c07'
   },
-  data : data
+  data : {
+  "data": {
+      
+      "Date_field" : date,
+      "Time" : time,
+      "Token" : token,
+      "Name" : name,
+      "WhatsApp" : whatsapp
+          }
+        }
 };
 
 axios(config)
